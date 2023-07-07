c9d68827c6a90bfa163c57e10e9e0c69e0db24ce
BLuchterhand <benlucht8@gmail.com>
2023-07-07 18:43:38 -0400

    Update account_test.rs

--- tests/account_test.rs
+++ tests/account_test.rs
@@ -83,20 +83,22 @@ async fn test_signup() -> Result<(), Box<dyn std::error::Error>> {
         "Your account has been created!".to_string()
     );
 
-    // let request_two = Request::<SignupRequest>::new(SignupRequest {
-    //     email: "test2@gmail.com".to_string(),
-    //     password: "4321".to_string(),
-    // });
+    client = AuthorizationClient::connect("http://0.0.0.0:40130").await?;
 
-    // let response = client
-    //     .signup(request_two)
-    //     .await
-    //     .map_err(|err| Status::unknown(err.to_string()))?;
+    let request_two = Request::<SignupRequest>::new(SignupRequest {
+        email: "test2@gmail.com".to_string(),
+        password: "4321".to_string(),
+    });
 
-    // assert_eq!(
-    //     response.into_inner().message,
-    //     "That email is already linked to an account.".to_string()
-    // );
+    let response = client
+        .signup(request_two)
+        .await
+        .map_err(|err| Status::unknown(err.to_string()))?;
+
+    assert_eq!(
+        response.into_inner().message,
+        "That email is already linked to an account.".to_string()
+    );
 
     let _result = db_client.client.execute(
         "delete from public.accounts where email = $1",
