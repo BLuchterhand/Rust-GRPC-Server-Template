49b86998b58afa2792d97ad3ccbe045a0e3fab21
BLuchterhand <benlucht8@gmail.com>
2023-07-07 18:30:51 -0400

    Update account_test.rs

--- tests/account_test.rs
+++ tests/account_test.rs
@@ -5,53 +5,53 @@ mod tests {
         store::DBClient, 
         authorization::{
             authorization_client::AuthorizationClient,
-            LoginRequest,
+            // LoginRequest,
             SignupRequest,
         },
     };
     use tonic::{Request, Status};
 
-#[tokio::test]
-async fn test_login() -> Result<(), Box<dyn std::error::Error>> {
-    let db_client = {
-        let (client, connection) = tokio_postgres::connect("postgresql://postgres:postgres@localhost:5432/postgres", NoTls).await?;
-        let db_client = DBClient::new(client);
-        tokio::spawn(async move {
-            if let Err(e) = connection.await {
-                eprintln!("connection error: {}", e);
-            }
-        });
-        db_client
-    };
+// #[tokio::test]
+// async fn test_login() -> Result<(), Box<dyn std::error::Error>> {
+//     let db_client = {
+//         let (client, connection) = tokio_postgres::connect("postgresql://postgres:postgres@localhost:5432/postgres", NoTls).await?;
+//         let db_client = DBClient::new(client);
+//         tokio::spawn(async move {
+//             if let Err(e) = connection.await {
+//                 eprintln!("connection error: {}", e);
+//             }
+//         });
+//         db_client
+//     };
 
-    let _result = db_client.client.execute(
-            "INSERT INTO public.accounts (id, email, password) VALUES($1, $2, $3)",
-            &[&10001i32, &"test1@gmail.com", &"$argon2id$v=19$m=19456,t=2,p=1$OZvRCPdpJUHXTly354KWHQ$LWpd6Bt7bqpAGKea0yhwU01UF5cdYUnkYY4xflkGRe4"],
-        ).await?;
+//     let _result = db_client.client.execute(
+//             "INSERT INTO public.accounts (id, email, password) VALUES($1, $2, $3)",
+//             &[&10001i32, &"test1@gmail.com", &"$argon2id$v=19$m=19456,t=2,p=1$OZvRCPdpJUHXTly354KWHQ$LWpd6Bt7bqpAGKea0yhwU01UF5cdYUnkYY4xflkGRe4"],
+//         ).await?;
 
-    let mut client = AuthorizationClient::connect("http://0.0.0.0:40130").await?;
+//     let mut client = AuthorizationClient::connect("http://0.0.0.0:40130").await?;
 
-    let request = Request::<LoginRequest>::new(LoginRequest {
-        email: "test1@gmail.com".to_string(),
-        password: "Hello".to_string(),
-    });
+//     let request = Request::<LoginRequest>::new(LoginRequest {
+//         email: "test1@gmail.com".to_string(),
+//         password: "Hello".to_string(),
+//     });
 
-    let response = client
-        .login(request)
-        .await
-        .map_err(|err| Status::unknown(err.to_string()))?;
+//     let response = client
+//         .login(request)
+//         .await
+//         .map_err(|err| Status::unknown(err.to_string()))?;
 
-    let _result = db_client.client.execute(
-        "delete from public.accounts where id = $1",
-            &[&10001i32],
-    ).await?;
+//     let _result = db_client.client.execute(
+//         "delete from public.accounts where id = $1",
+//             &[&10001i32],
+//     ).await?;
 
-    assert_eq!(
-        response.into_inner().status,
-        true
-    );
-    Ok(())
-    }
+//     assert_eq!(
+//         response.into_inner().status,
+//         true
+//     );
+//     Ok(())
+// }
 
 #[tokio::test]
 async fn test_signup() -> Result<(), Box<dyn std::error::Error>> {
